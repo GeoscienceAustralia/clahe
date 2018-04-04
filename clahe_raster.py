5eb61e8e338afe0ea03faea648a60a77a13ba436
Sudipta Basak <basaks@gmail.com>
2018-04-05 05:16:45 +1000

    minor change in comments

--- clahe_raster.py
+++ clahe_raster.py
@@ -14,19 +14,28 @@ src = rio.open(input_raster)
 data = src.read(1, masked=True)  # read band 1
 
 
-# rescale the data as the adapthist expects data in (-1, 1)
-# specify in_range=(min_value_to_limit, max_value_to_limit), 
-# in_range="image" to use min and max in the image values.
+"""
+rescale the data as the adapthist expects data in (-1, 1)
+specify in_range=(min_value_to_limit, max_value_to_limit), 
+in_range="image" to use min and max in the image values.
+"""
+
 resacled_data = rescale_intensity(data, in_range="image", out_range=(-1, 1))
 
-# kernel_size: integer or list-like, optional
-# Defines the shape of contextual regions used in the algorithm. If iterable is passed, it must have the same number of elements as image.ndim (without color channel). If integer, it is broadcasted to each image dimension. By default, kernel_size is 1/8 of image height by 1/8 of its width.
+"""
+kernel_size: integer or list-like, optional
+Defines the shape of contextual regions used in the algorithm. 
+If iterable is passed, it must have the same number of elements 
+as image.ndim (without color channel). 
+If integer, it is broadcasted to each image dimension. By default, 
+kernel_size is 1/8 of image height by 1/8 of its width.
  
-# clip_limit : float, optional
-# Clipping limit, normalized between 0 and 1 (higher values give more contrast).
+clip_limit : float, optional
+Clipping limit, normalized between 0 and 1 (higher values give more contrast).
  
-# nbins : int, optional
-# Number of gray bins for histogram (“data range”).
+nbins : int, optional
+Number of gray bins for histogram (“data range”).
+"""
 
 stretched_data = equalize_adapthist(resacled_data,
                                     kernel_size=200,
