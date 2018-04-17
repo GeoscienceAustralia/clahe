0839944665dc5c1bb2e15c29fc5fdf443c4e5b07
Sudipta Basak <basaks@gmail.com>
2018-04-18 09:01:02 +1000

    use skimage rescale intensity to scale data into rgb

--- rgb_combine.py
+++ rgb_combine.py
@@ -1,5 +1,7 @@
 import numpy as np
 from osgeo import gdal, gdalconst
+from skimage.exposure import rescale_intensity
+
 
 def read_geotif(tif):
     src = gdal.Open(tif, gdalconst.GA_ReadOnly)
@@ -12,19 +14,17 @@ def read_geotif(tif):
                              fill_value=nodata)
 
 
-def multiscale(local, meso, broad, input_tif, output_tif, cutoff=2.58):
+def rgb_combine(local, meso, broad, input_tif, output_tif):
 
     loc = read_geotif(local)
     mes = read_geotif(meso)
     bro = read_geotif(broad)
 
-    # standardise and take absolute, and scale by cutoff
-    loc = (np.ma.abs(loc - loc.mean()) / loc.std()) * 255/cutoff
-    mes = (np.ma.abs(mes - mes.mean()) / mes.std()) * 255/cutoff
-    bro = (np.ma.abs(bro - bro.mean()) / bro.std()) * 255/cutoff
+    # rescale to 0-256, 256 because we later save as gdt_byte
+    loc = rescale_intensity(loc, out_range=(0, 256))
+    mes = rescale_intensity(mes, out_range=(0, 256))
+    bro = rescale_intensity(bro, out_range=(0, 256))
 
-
-    # import IPython; IPython.embed(); import sys; sys.exit()
     # source information
     src_ds = gdal.Open(input_tif, gdalconst.GA_ReadOnly)
 
@@ -49,5 +49,5 @@ def multiscale(local, meso, broad, input_tif, output_tif, cutoff=2.58):
 
 
 if __name__ == '__main__':
-    multiscale('out_mb.tif', 'out_mb1.tif', 'out_mb2.tif', 'out_mb.tif',
-               'rgb.tif')
+    rgb_combine('out_mb.tif', 'out_mb1.tif', 'out_mb2.tif', 'out_mb.tif',
+                'rgb.tif')
